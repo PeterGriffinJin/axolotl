066b9fe333ca861e6e8d77d207b96887ad13f087
PeterGriffinJin <bowenj4@illinois.edu>
2024-09-16 20:39:50 +0000

    update yamls

---
+++ run1.sh
@@ -0,0 +1,9 @@
+
+CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/mistral-nemo/fft-nemo-mixdata.yaml
+accelerate launch -m axolotl.cli.train examples/mistral-nemo/fft-nemo-mixdata.yaml --deepspeed deepspeed_configs/zero3.json
+
+CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/mistral-nemo/fft-nemo-mixdata-rationale.yaml
+accelerate launch -m axolotl.cli.train examples/mistral-nemo/fft-nemo-mixdata-rationale.yaml --deepspeed deepspeed_configs/zero3.json
+
+CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/mistral-nemo/fft-nemo-mixdata-direct.yaml
+accelerate launch -m axolotl.cli.train examples/mistral-nemo/fft-nemo-mixdata-direct.yaml --deepspeed deepspeed_configs/zero3.json
---
+++ run2.sh
@@ -0,0 +1,9 @@
+
+CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/llama-3/fft-8b-mixdata.yaml
+accelerate launch -m axolotl.cli.train examples/llama-3/fft-8b-mixdata.yaml --deepspeed deepspeed_configs/zero3.json
+
+CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/llama-3/fft-8b-mixdata-rationale.yaml
+accelerate launch -m axolotl.cli.train examples/llama-3/fft-8b-mixdata-rationale.yaml --deepspeed deepspeed_configs/zero3.json
+
+CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/llama-3/fft-8b-mixdata-direct.yaml
+accelerate launch -m axolotl.cli.train examples/llama-3/fft-8b-mixdata-direct.yaml --deepspeed deepspeed_configs/zero3.json
