ac966ee804619c984f9ea06c14d3db3aad173a15
PeterGriffinJin <bowenj4@illinois.edu>
2024-12-20 19:24:47 +0000

    add mistral/llama train script

---
+++ run_llama.sh
@@ -0,0 +1,14 @@
+
+# implicit rag finetune
+CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/llama-3/fft-8b-mixdata.yaml
+accelerate launch -m axolotl.cli.train examples/llama-3/fft-8b-mixdata.yaml --deepspeed deepspeed_configs/zero3.json
+
+# explicit rag finetune
+CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/llama-3/fft-8b-mixdata-rationale.yaml
+accelerate launch -m axolotl.cli.train examples/llama-3/fft-8b-mixdata-rationale.yaml --deepspeed deepspeed_configs/zero3.json
+
+# CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/llama-3/fft-8b-mixdata-direct.yaml
+# accelerate launch -m axolotl.cli.train examples/llama-3/fft-8b-mixdata-direct.yaml --deepspeed deepspeed_configs/zero3.json
+
+# CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/llama-3/fft-8b-mixdata-rationale-self.yaml
+# accelerate launch -m axolotl.cli.train examples/llama-3/fft-8b-mixdata-rationale-self.yaml --deepspeed deepspeed_configs/zero3.json
--- run_mistral.sh
+++ run_mistral.sh
@@ -1,9 +1,11 @@
 
+# implicit rag finetune
 CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/mistral-nemo/fft-nemo-mixdata.yaml
 accelerate launch -m axolotl.cli.train examples/mistral-nemo/fft-nemo-mixdata.yaml --deepspeed deepspeed_configs/zero3.json
 
+# explicit rag finetune
 CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/mistral-nemo/fft-nemo-mixdata-rationale.yaml
 accelerate launch -m axolotl.cli.train examples/mistral-nemo/fft-nemo-mixdata-rationale.yaml --deepspeed deepspeed_configs/zero3.json
 
-CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/mistral-nemo/fft-nemo-mixdata-direct.yaml
-accelerate launch -m axolotl.cli.train examples/mistral-nemo/fft-nemo-mixdata-direct.yaml --deepspeed deepspeed_configs/zero3.json
+# CUDA_VISIBLE_DEVICES=0 python -m axolotl.cli.preprocess examples/mistral-nemo/fft-nemo-mixdata-direct.yaml
+# accelerate launch -m axolotl.cli.train examples/mistral-nemo/fft-nemo-mixdata-direct.yaml --deepspeed deepspeed_configs/zero3.json
