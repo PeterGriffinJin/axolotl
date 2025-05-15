8dc7aaf52d003ae5a1604004795d54ccda730871
PeterGriffinJin <bowenj4@illinois.edu>
2025-05-15 00:49:27 +0000

    update config

--- process_data.py
+++ process_data.py
@@ -1,12 +1,12 @@
 import json
 
-with open('/home/peterjin/mnt/other_data/cell-o1/train_raw.json', 'r') as f:
+with open('/home/peterjin/mnt/other_data/cell-o1/sft_reasoning_raw.json', 'r') as f:
     raw_data = json.load(f)
 
 print(len(raw_data))
-with open('/home/peterjin/mnt/other_data/cell-o1/train.jsonl', 'w') as fout:
+with open('/home/peterjin/mnt/other_data/cell-o1/sft_reasoning.jsonl', 'w') as fout:
     for d in raw_data:
         fout.write(json.dumps({"messages": [{"role": "system", "content": d['system_msg']}, 
                                             {"role": "user", "content": f"{d['user_msg']}\nAnswer:"}, 
-                                            {"role": "assistant", "content": d['answer']}]
+                                            {"role": "assistant", "content": d['assistant_msg']}]
                                 }) + '\n')
